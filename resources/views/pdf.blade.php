b95bfed2129348a05fb145901ffe7e69bc2b8321
Polash Mahmud <polashmahmud@gmail.com>
2017-11-06 17:23:28 +0600

    setup laravel

---
+++ resources/views/pdf.blade.php
@@ -0,0 +1,75 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<style>
+@font-face {
+  font-family: 'FontAwesome';
+  src: url('../fonts/fontawesome-webfont.eot?v=4.7.0');
+  src: url('../fonts/fontawesome-webfont.eot?#iefix&v=4.7.0') format('embedded-opentype'), url('../fonts/fontawesome-webfont.woff2?v=4.7.0') format('woff2'), url('../fonts/fontawesome-webfont.woff?v=4.7.0') format('woff'), url('../fonts/fontawesome-webfont.ttf?v=4.7.0') format('truetype'), url('../fonts/fontawesome-webfont.svg?v=4.7.0#fontawesomeregular') format('svg');
+  font-weight: normal;
+  font-style: normal;
+}
+
+@font-face {
+font-family: 'hind_siliguribold';
+src: url('../fonts/hindsiliguri-bold-webfont.woff2') format('woff2'),
+     url('../fonts/hindsiliguri-bold-webfont.woff') format('woff');
+font-weight: normal;
+font-style: normal;
+
+}
+
+@font-face {
+    font-family: 'hind_siligurilight';
+    src: url('../fonts/hindsiliguri-light-webfont.woff2') format('woff2'),
+         url('../fonts/hindsiliguri-light-webfont.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+
+}
+
+@font-face {
+    font-family: 'hind_siligurimedium';
+    src: url('../fonts/hindsiliguri-medium-webfont.woff2') format('woff2'),
+         url('../fonts/hindsiliguri-medium-webfont.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+
+}
+
+@font-face {
+    font-family: 'hind_siliguriregular';
+    src: url('../fonts/hindsiliguri-regular-webfont.woff2') format('woff2'),
+         url('../fonts/hindsiliguri-regular-webfont.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+
+}
+
+@font-face {
+    font-family: 'hind_siligurisemibold';
+    src: url('../fonts/hindsiliguri-semibold-webfont.woff2') format('woff2'),
+         url('../fonts/hindsiliguri-semibold-webfont.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+
+}
+
+.fa {
+  display: inline-block;
+  font: normal normal normal 14px/1 FontAwesome;
+  font-size: inherit;
+  text-rendering: auto;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.fa-dashboard:before {
+  content: "\f0e4";
+}
+</style>
+
+<span>
+	<i class="fa fa-dashboard" style="font-size: 80px"></i>
+	<p style="font-family: Hind Siliguri; font-weight: 400; font-size: 30px;">
+		২০০১ খ্রিস্টাব্দের জাতীয় সংসদ নির্বাচনে বিশাল জয়ের মধ্য দিয়ে বাংলাদেশ জাতীয়তাবাদী দল (বিএনপি) সরকার গঠন করে এবং খালেদা জিয়া পুনরায় প্রধানমন্ত্রী হন। তিনি ২০০১ থেকে ২০০৬ খ্রিস্টাব্দ মেয়াদে প্রধানমন্ত্রীর দায়িত্ব পালন করেন। অতঃপর নানা নাটকীয় পালা বদলের মধ্য দিয়ে প্রধান উপদেষ্টা হিসাবে ফখরুদ্দিন আহমদ তত্ত্বাবধায়ক 
+	</p>
+</span>
